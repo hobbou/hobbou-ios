d2a9326bbce865b322cf8b59e45c296894dfb743
Hans Yonathan <hansyonathan@Hanss-MacBook-Pro.local>
2017-01-10 10:25:11 +0700

    [IMP]small improvement;

--- hobbou/AdventureCellView.swift
+++ hobbou/AdventureCellView.swift
@@ -98,19 +98,81 @@ class CategorySelectionView: UIView, UICollectionViewDataSource, UICollectionVie
     
 }
 
-class FeaturedListCell: BaseCell {
+class FeaturedListCell: BaseCell, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
+    let featuredCellId = "featuredCellId"
     
     let pager: UIView = {
         let view = UIView()
         view.backgroundColor = .green
         return view
     }()
-        
-    override func setupViews() {
-        addSubview(pager)
-        addConstraint(format: "H:|[v0]|", views: pager)
-        addConstraint(format: "V:|[v0]|", views: pager)
+
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.delegate = self
+        cv.dataSource = self
+        cv.backgroundColor = .clear
+        return cv
+    }()
+    
+    lazy var pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.pageIndicatorTintColor = .lightGray
+        pc.currentPageIndicatorTintColor = UIColor(red: 247/255, green: 154/255, blue: 27/255, alpha: 1)
+        pc.addTarget(self, action: #selector(handlePageControlChange), for: .valueChanged)
+        pc.numberOfPages = 4
+        pc.translatesAutoresizingMaskIntoConstraints = false
+        return pc
+    }()
+    
+    func handlePageControlChange(){
+        print(pageControl.currentPage)
+        collectionView.scrollToItem(at: IndexPath(item: pageControl.currentPage, section: 0), at: .init(rawValue: 0), animated: true)
     }
+    
+    override func setupViews() {
+        addSubview(collectionView)
+        addConstraint(format: "H:|[v0]|", views: collectionView)
+        addConstraint(format: "V:|[v0]|", views: collectionView)
+
+        addSubview(pageControl)
+        pageControl.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        pageControl.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        pageControl.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        //height constraint
+        addConstraint(NSLayoutConstraint(item: pageControl, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 30))
+        
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.scrollDirection = .horizontal
+            flowLayout.minimumLineSpacing = 0
+        }
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.alwaysBounceHorizontal = true
+        collectionView.isPagingEnabled = true
+        collectionView.register(FeaturedCell.self, forCellWithReuseIdentifier: featuredCellId)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: featuredCellId, for: indexPath) as! FeaturedCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let height = (frame.width - 16 - 16) * 9 / 16
+        return CGSize(width: frame.width, height: (height + 16 + 68))
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let pageNumber = Int(targetContentOffset.pointee.x / frame.width)
+        pageControl.currentPage = pageNumber
+    }
+    
 }
 
 class RecomendedListCell: BaseCell {
@@ -207,6 +269,24 @@ class CategoryListCell: BaseCell {
         containerView.addSubview(categLabel)
         addConstraint(format: "H:|[v0]|", views: categLabel)
         addConstraint(format: "V:|[v0]|", views: categLabel)
+    }
+    
+}
+
+class FeaturedCell: BaseCell {
+    
+    let thumbnailImageView: CustomImageView = {
+        let imageView = CustomImageView()
+        imageView.backgroundColor = UIColor.blue
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override func setupViews() {
+        addSubview(thumbnailImageView)
+        addConstraint(format: "H:|[v0]|", views: thumbnailImageView)
+        addConstraint(format: "V:|[v0]|", views: thumbnailImageView)
     }
     
 }
